a9e6fd805eb869bf1ed7bcbe6867a19f6b25e652
Jonathon Anderson <juke1229@gmail.com>
2020-03-01 18:58:28 -0500

    Change code

--- index.js
+++ index.js
@@ -1,30 +1,69 @@
 var map;
+var icon = "http://path/to/icon.png";
+var json = "http://path/to/universities.json";
+var infowindow = new google.maps.InfoWindow();
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 35.492254, lng: -82.526401}, 
-        zoom: 10
-    })
-    var script = map.data.loadGeoJson('map.json');
-    document.getElementsByTagName('head')[0].appendChild(script)
-}
-var window = function (results) {
-    for (var i = 0; i < results.features.length; i++) {
-        const coords = results.features[i].geometry.coordinates;
-        const latLng = new google.maps.LatLng(coords[1],coords[0]);
-        const marker = new google.maps.Marker({
+
+    var mapProp = {
+        center: new google.maps.LatLng(52.4550, -3.3833), //LLANDRINDOD WELLS
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("map"), mapProp);
+
+      //  $.getJSON(json, function(json1) {
+    var json1 = {
+        "universities": [{
+            "title": "Aberystwyth University",
+            "website": "www.aber.ac.uk",
+            "phone": "+44 (0)1970 623 111",
+            "lat": 52.415524,
+            "lng": -4.063066
+        },
+        {
+            "title": "Bangor University",
+            "website": "www.bangor.ac.uk",
+            "phone": "+44 (0)1248 351 151",
+            "lat": 53.229520,
+            "lng": -4.129987
+        },
+        {
+            "title": "Cardiff Metropolitan University",
+            "website": "www.cardiffmet.ac.uk",
+            "phone": "+44 (0)2920 416 138",
+            "lat": 51.482708,
+            "lng": -3.165881
+        }
+        ]
+    };
+    $.each(json1.universities, function(key, data) {
+
+        var latLng = new google.maps.LatLng(data.lat, data.lng);
+
+        var marker = new google.maps.Marker({
             position: latLng,
-            map: map
+            map: map,
+          // icon: icon,
+            title: data.title
         });
 
-        var infowindow = new google.maps.InfoWindow({
-            content: 'click me',
-            position: latLng
+        var details = data.website + ", " + data.phone + ".";
+
+        bindInfoWindow(marker, map, infowindow, details);
+
+        //    });
+
         });
 
-        infowindow.open(map)
-        window.marker.addListener('click', function() {
-            infowindow.setContent('Zoom: ');
-        });
     }
+
+    function bindInfoWindow(marker, map, infowindow, strDescription) {
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(strDescription);
+            infowindow.open(map, marker);
+        });
 }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
