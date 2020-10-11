bd643804c14499b03fd775c68318b6eaf8b43088
Jonathon Anderson <juke1229@gmail.com>
2020-10-10 23:16:28 -0400

    Virginia fixed

--- Virginia/vaWaterfalls.js
+++ Virginia/vaWaterfalls.js
@@ -74,10 +74,8 @@ function initMap() {
         // When Marker clicked
         //TODO (later) Make More pretty
         var details = `<div class="marker" ><strong><h3>${data.title}</h3></strong> 
-                        Beauty Rating: <span class="number">${data.beauty}</span> Photo Rating: <span class="number">${data.photo}</span>
-                        <br> Hike Difficulty: <span class="number">${data.hDifficulty}</span> Hike Distance: <span class="number">${data.hDistance}</span>
-                        <br> Compass Heading: <span class="number">${data.compass}</span> Elevation: <span class="number">${data.elevation}</span>
-                        <br> Page Number: <span class="number">page ${data.pageNumber}</span> Canopy: <span class="number">${data.canopy}</span><div>
+                        Hike Difficulty: <span class="number">${data.hDifficulty}</span> Hike Distance: <span class="number">${data.hDistance}</span>
+                        <br> Page Number: <span class="number">page ${data.pageNumber}</span><div>
                         <a href="https://www.google.com/maps/dir/?api=1&destination=${data.lat},${data.lng}" target="_blank" class="button" style="margin-top:100px">Click Here For Directions</a>
                         `
 
@@ -106,46 +104,32 @@ google.maps.event.addDomListener(window, 'load', initMap);
 
 window.onload=function(){
     function logSubmit(event) {
-        let bR = document.getElementById("beautyRating_input").value
-        let pR = document.getElementById("photoRating_input").value
+        // let bR = document.getElementById("beautyRating_input").value
+        // let pR = document.getElementById("photoRating_input").value
         let hD = document.getElementById("hikeDifficulty_input").value
-        let elv = document.getElementById("elevation_input").value
-        let can = document.getElementById("canopy_input").value
+        // let elv = document.getElementById("elevation_input").value
+        // let can = document.getElementById("canopy_input").value
         let hDis = document.getElementById("hikeDistance_input").value
-        let comp = document.getElementById("compass_input").value
+        // let comp = document.getElementById("compass_input").value
 
-        if (elv == 0) {
-            elv = 10000
-        }
         if (hD == 0) {
             hD = 10
         }
 
-        console.log(bR, pR, hD, elv, can, hDis, comp)
+        // console.log(bR, pR, hD, elv, can, hDis, comp)
 
         const filteredList = array => array
-            .filter(location => location.beauty >= bR)
-            .filter(location => location.photo >= pR)
+            // .filter(location => location.beauty >= bR)
+            // .filter(location => location.photo >= pR)
             .filter(location => location.hDifficulty <= hD)
-            .filter(location => location.elevation <= elv)
+            // .filter(location => location.elevation <= elv)
             // .filter(location => location.canopy = can)
-
-        const filteredListCan = array => array
-            .filter(location => location.beauty >= bR)
-            .filter(location => location.photo >= pR)
-            .filter(location => location.hDifficulty <= hD)
-            .filter(location => location.elevation <= elv)
-            .filter(location => location.canopy == can)
 
         let finalList = []
 
-        if (can == 'all') {
-            finalList = filteredList(json.waterfalls)
-        } else {
-            finalList = filteredListCan(json.waterfalls)
-        }
+        finalList = filteredList(json.waterfalls)
 
-        //Hike Distance
+        //* Hike Distance
         switch (hDis) {
             case "1":
                 finalList = lessThanTenth(finalList)
@@ -173,34 +157,34 @@ window.onload=function(){
         };
 
         // Compass Heading 
-        switch (comp) {
-            case 'N':
-                finalList = N(finalList)
-                break
-            case 'NE':
-                finalList = NE(finalList)
-                break
-            case 'E':
-                finalList = E(finalList)
-                break
-            case 'SE':
-                finalList = SE(finalList)
-                break
-            case 'S':
-                finalList = S(finalList)
-                break
-            case 'SW':
-                finalList = SW(finalList)
-                break
-            case 'W':
-                finalList = W(finalList)
-                break
-            case 'NW':
-                finalList = NW(finalList)
-                break
-            default:
-                finalList = finalList
-        }
+        // switch (comp) {
+        //     case 'N':
+        //         finalList = N(finalList)
+        //         break
+        //     case 'NE':
+        //         finalList = NE(finalList)
+        //         break
+        //     case 'E':
+        //         finalList = E(finalList)
+        //         break
+        //     case 'SE':
+        //         finalList = SE(finalList)
+        //         break
+        //     case 'S':
+        //         finalList = S(finalList)
+        //         break
+        //     case 'SW':
+        //         finalList = SW(finalList)
+        //         break
+        //     case 'W':
+        //         finalList = W(finalList)
+        //         break
+        //     case 'NW':
+        //         finalList = NW(finalList)
+        //         break
+        //     default:
+        //         finalList = finalList
+        // }
 
         event.preventDefault()
 
@@ -213,15 +197,15 @@ window.onload=function(){
 }
 
 
-//* For Compass Heading
-const N = array => array.filter(location => location.compass >=338 || location.compass <=22)
-const NE = array => array.filter(location => location.compass >=23 && location.compass <=67)
-const E = array => array.filter(location => location.compass >=68 && location.compass <=112)
-const SE = array => array.filter(location => location.compass >=113 && location.compass <=157)
-const S = array => array.filter(location => location.compass >=158 && location.compass <=202)
-const SW = array => array.filter(location => location.compass >=203 && location.compass <=247)
-const W = array => array.filter(location => location.compass >=248 && location.compass <=292)
-const NW = array => array.filter(location => location.compass >=293 && location.compass <=337)
+// //* For Compass Heading
+// const N = array => array.filter(location => location.compass >=338 || location.compass <=22)
+// const NE = array => array.filter(location => location.compass >=23 && location.compass <=67)
+// const E = array => array.filter(location => location.compass >=68 && location.compass <=112)
+// const SE = array => array.filter(location => location.compass >=113 && location.compass <=157)
+// const S = array => array.filter(location => location.compass >=158 && location.compass <=202)
+// const SW = array => array.filter(location => location.compass >=203 && location.compass <=247)
+// const W = array => array.filter(location => location.compass >=248 && location.compass <=292)
+// const NW = array => array.filter(location => location.compass >=293 && location.compass <=337)
 
 //* For Hike Distance
 const lessThanTenth = array => array.filter(location => location.hDistance.split(' ')[1] == "yards" || location.hDistance.split(' ')[1] == "feet" || location.hDistance.split(' ')[0] <= 0.1)
